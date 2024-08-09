cb2553a475869c520dea6ad41d79f62999549a3c
andref-dev <andre.hl2@outlook.com>
2024-08-09 13:28:49 -0300

    - Fix issues with LockKeeper integration

--- lock_keeper.go
+++ lock_keeper.go
@@ -8,8 +8,8 @@ import (
 	"io"
 	"net/http"
 	"net/url"
-	"os"
 
+	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/signer/core/apitypes"
 )
 
@@ -17,114 +17,121 @@ func Login() (string, error) {
 	//TODO: Move these to environment variables
 	lockKeeperURL := "http://localhost:3000"
 	tenantName := "Game7"
+	serviceProviderUsername := "game7_service_provider"
+	serviceProviderPassword := "password"
 
 	//TODO: Move these to environment variables
 	loginData := url.Values{
-		"username":   {"game7_service_provider"},
-		"password":   {"password"},
+		"username":   {serviceProviderUsername},
+		"password":   {serviceProviderPassword},
 		"grant_type": {"password"},
 	}
 
-	res, err := http.PostForm(
+	res, postErr := http.PostForm(
 		fmt.Sprintf("%s/%s/login", lockKeeperURL, tenantName),
 		loginData,
 	)
-	if err != nil {
-		os.Stdout.Write([]byte("Error making login request: " + err.Error() + "\n"))
-		return "", err
+	if postErr != nil {
+		return "", postErr
 	}
 	defer res.Body.Close()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		os.Stdout.Write([]byte("Error reading login response body: " + err.Error() + "\n"))
-		return "", err
+	body, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		return "", readErr
 	}
 
-	// Parse the JSON response to get the access token
 	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		os.Stdout.Write([]byte("Error decoding login JSON response: " + err.Error() + "\n"))
-		return "", err
+	if deserializeErr := json.Unmarshal(body, &result); deserializeErr != nil {
+		return "", deserializeErr
 	}
 
 	accessToken := result["access_token"].(string)
-
 	return accessToken, nil
 }
 
 func SignTypedMessage(message apitypes.TypedData, keyId string, accessToken string) (string, error) {
-	lockKeeperURL := "http://localhost:3000" // replace with actual URL
+	// TODO: Move this to environment variable
+	lockKeeperURL := "http://localhost:3000"
+	policyName := "noop_policy"
 
-	// Encode message to JSON
-	jsonBytes, err := json.Marshal(message)
-	if err != nil {
-		os.Stdout.Write([]byte("Error encoding message to JSON: " + err.Error() + "\n"))
-		return "", err
+	jsonBytes, serializeErr := SerializeTypedDataWithoutSalt(message)
+	if serializeErr != nil {
+		return "", serializeErr
 	}
-
-	os.Stdout.Write(jsonBytes)
-
-	// Encode JSON to base64
 	typedData := base64.StdEncoding.EncodeToString(jsonBytes)
 
-	os.Stdout.Write([]byte(typedData))
-
-	// Prepare request body
 	reqBody := map[string]interface{}{
 		"typed_data":       typedData,
 		"authorizing_data": []interface{}{},
 		"key_id":           keyId,
-		"message_type":     "Erc2771",
-		"policies":         []string{"noop_policy"},
+		"message_type":     "Standard",
+		"policies":         []string{policyName},
 	}
 
-	// Encode request body to JSON
-	reqBodyJson, err := json.Marshal(reqBody)
-	if err != nil {
-		os.Stdout.Write([]byte("Error encoding request body to JSON: " + err.Error() + "\n"))
-		return "", err
+	reqBodyJson, serializeErr := json.Marshal(reqBody)
+	if serializeErr != nil {
+		return "", serializeErr
 	}
 
-	// Make the POST request
-	req, err := http.NewRequest("POST", lockKeeperURL+"/sign_message", bytes.NewBuffer(reqBodyJson))
-	if err != nil {
-		os.Stdout.Write([]byte("Error creating POST request: " + err.Error() + "\n"))
-		return "", err
+	req, reqErr := http.NewRequest("POST", lockKeeperURL+"/sign_message", bytes.NewBuffer(reqBodyJson))
+	if reqErr != nil {
+		return "", reqErr
 	}
-
-	// Set headers
 	req.Header.Set("Authorization", "Bearer "+accessToken)
 	req.Header.Set("Content-Type", "application/json")
 
-	// Send the request
 	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		os.Stdout.Write([]byte("Error making POST request: " + err.Error() + "\n"))
-		return "", err
+	resp, postErr := client.Do(req)
+	if postErr != nil {
+		return "", postErr
 	}
 	defer resp.Body.Close()
 
-	// Read the response body
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		os.Stdout.Write([]byte("Error reading response body: " + err.Error() + "\n"))
-		return "", err
+	body, readErr := io.ReadAll(resp.Body)
+	if readErr != nil {
+		return "", readErr
 	}
 
-	// Print the body text
-	os.Stdout.Write(body)
-
-	// Parse the JSON response
 	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		os.Stdout.Write([]byte("Error decoding JSON response: " + err.Error() + "\n"))
-		return "", err
+	if deserializeErr := json.Unmarshal(body, &result); deserializeErr != nil {
+		return "", deserializeErr
 	}
 
-	// Extract the signature from the response
 	signature := result["signature"].(string)
-
 	return signature, nil
+}
+
+// In order to sign the typed message with LockKeeper, we need to serialize
+// the typed data message without the salt field in the 'domain' object.
+type CustomDomain struct {
+	Name              string                `json:"name,omitempty"`
+	Version           string                `json:"version,omitempty"`
+	ChainId           *math.HexOrDecimal256 `json:"chainId,omitempty"`
+	VerifyingContract string                `json:"verifyingContract,omitempty"`
+}
+
+type CustomTypedData struct {
+	Types       map[string][]apitypes.Type `json:"types"`
+	PrimaryType string                     `json:"primaryType"`
+	Domain      CustomDomain               `json:"domain"`
+	Message     map[string]interface{}     `json:"message"`
+}
+
+func SerializeTypedDataWithoutSalt(original apitypes.TypedData) ([]byte, error) {
+	customDomain := CustomDomain{
+		Name:              original.Domain.Name,
+		Version:           original.Domain.Version,
+		ChainId:           original.Domain.ChainId,
+		VerifyingContract: original.Domain.VerifyingContract,
+	}
+
+	customTypedData := CustomTypedData{
+		Types:       original.Types,
+		PrimaryType: original.PrimaryType,
+		Domain:      customDomain,
+		Message:     original.Message,
+	}
+
+	return json.Marshal(customTypedData)
 }
